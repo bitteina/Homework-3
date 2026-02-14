2ac74ef1c2a4c1332a2bee603faa13fb1f034399
bitteina <banskihanski@gmail.com>
2026-02-14 22:34:16 +0200

    Separate thumb and index animations

--- Homework 3/Assets/HandController.cs
+++ Homework 3/Assets/HandController.cs
@@ -4,6 +4,8 @@ public class HandController : MonoBehaviour
 {
     public InputActionReference gripInput;
     public InputActionReference triggerInput;
+    public InputActionReference indexInput;
+    public InputActionReference thumbInput;
 
     private Animator animator;
 
@@ -17,9 +19,13 @@ public class HandController : MonoBehaviour
         if (!animator) return;
         float grip = gripInput.action.ReadValue<float>();
         float trigger = triggerInput.action.ReadValue<float>();
+        float indexTouch = indexInput.action.ReadValue<float>();
+        float thumbTouch = thumbInput.action.ReadValue<float>();
 
         animator.SetFloat("Grip", grip);
         animator.SetFloat("Trigger", trigger);
+        animator.SetFloat("Index", indexTouch);
+        animator.SetFloat("Thumb", thumbTouch);
 
     }
 }
