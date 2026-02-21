afd69b17cace00b255b21a2af5b19e6f66264500
bitteina <banskihanski@gmail.com>
2026-02-21 19:11:44 +0200

    Added XR Interaction Manager, tried implementing grabbing (needs to be tested)

--- Homework 3/Assets/HandPresencePhysics.cs
+++ Homework 3/Assets/HandPresencePhysics.cs
@@ -7,14 +7,32 @@ public class HandPresencePhysics : MonoBehaviour
 
     public Transform target;
     private Rigidbody rb;
-
+    private Collider[] handColliders;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        handColliders = GetComponentsInChildren<Collider>();
     }
 
+    public void EnableHandCollider()
+    {
+        foreach (var item in handColliders) {
+            item.enabled = true;
+        }
+    }
+
+    public void EnableHandColliderDelay(float delay)
+    {
+        Invoke("EnableHandCollider", delay);
+    }
+    public void DisableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
